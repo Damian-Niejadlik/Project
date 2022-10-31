9a0565177daf8c49a8384de3c2dfadc1f5493edb
Omikix <omikix@gmail.com>
2022-10-31 14:02:56 +0100

    redirect fix and change templates

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from register.views import BaseView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('image/', include("aplikacja.urls")),
     path('user/', include("register.urls")),
+    path('', BaseView.as_view()),
 ]
